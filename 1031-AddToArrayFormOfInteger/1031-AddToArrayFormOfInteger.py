b12a0bf2bf65c67948f0e23438aaa768eca30ffa
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2026-02-04 12:16:01 -0500

    Create 1031-AddToArrayFormOfInteger

---
+++ 1031-AddToArrayFormOfInteger/1031-AddToArrayFormOfInteger.py
@@ -0,0 +1,14 @@
+# Last updated: 2/4/2026, 12:16:01 PM
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        carry = k
+        i = len(num)-1
+        while carry and i >= 0:
+            a = num[i] + carry
+            num[i] = a % 10
+            carry = a // 10
+            i -= 1
+        while carry:
+            num = [carry % 10] + num
+            carry = carry // 10
+        return num
