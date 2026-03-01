2271214d1d7ccf4f39eb4b73dcb7a899a883e928
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2026-03-01 14:59:49 -0500

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/version/python/128-LongestConsecutiveSequence_20260301_145949.py
@@ -0,0 +1,17 @@
+# Last updated: 3/1/2026, 2:59:49 PM
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        result = [0] * n
+        stack = []  # stores indexes
+
+        for i in range(n):
+
+            # resolve colder previous days
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                result[prev] = i - prev
+
+            stack.append(i)
+
+        return result
