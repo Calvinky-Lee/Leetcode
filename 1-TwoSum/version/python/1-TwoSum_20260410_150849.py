d29dca7b651103ff41aad11420f3a7c29b4686fd
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2026-04-10 15:08:49 -0400

    Create 1-TwoSum

---
+++ 1-TwoSum/version/python/1-TwoSum_20260410_150849.py
@@ -0,0 +1,12 @@
+# Last updated: 4/10/2026, 3:08:49 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        numbers = {}
+4        for i,n in enumerate(nums):
+5
+6            if target - n in numbers:
+7                return [i , numbers[target-n] ]
+8            
+9            numbers[n] = i
+10            
+11
