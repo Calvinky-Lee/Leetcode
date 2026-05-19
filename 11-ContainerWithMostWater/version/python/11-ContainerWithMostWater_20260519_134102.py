96e37aa3bfc60b867f4a37bd144030663ee5bd81
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2026-05-19 13:41:02 -0400

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/version/python/11-ContainerWithMostWater_20260519_134102.py
@@ -0,0 +1,16 @@
+# Last updated: 5/19/2026, 1:41:02 PM
+1class Solution:
+2    def maxArea(self, height: List[int]) -> int:
+3        left = 0
+4        right = len(height) - 1
+5        maxarea = 0 
+6        while right >= left:
+7            maxarea = max(maxarea, (right - left) * min(height[left], height[right]))
+8            #print(maxarea, right - left * min(height[left], height[right]))
+9            if height[right] > height[left]:
+10                left +=1
+11            else:
+12                right -= 1
+13        
+14        return maxarea
+15
