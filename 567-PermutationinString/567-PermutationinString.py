b8741c0badf9baec6fe52154787b69f0205ee1a3
Calvin Lee <63270424+Calvinky-Lee@users.noreply.github.com>
2026-07-28 17:38:19 -0400

    File updated at 7/28/2026, 5:38:19 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,14 +1,12 @@
-# Last updated: 7/28/2026, 4:57:28 PM
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-9        if p is None or q is None:
-10            return (p is None) and (q is None)
-11        else:
-12            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-13        
+# Last updated: 7/28/2026, 5:38:19 PM
+1class Solution:
+2    def subsets(self, nums: List[int]) -> List[List[int]]:
+3        output = [[]]
+4        while nums != []:
+5            adder = nums[0]
+6            for i in range(len(output)):
+7                new_add = output[i][:]
+8                new_add.append(adder)
+9                output.append(new_add)
+10            nums.pop(0)
+11        return output
